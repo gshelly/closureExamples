f03bdde5e28c698e02d3f30e5befd9f7f559cbce
Shelly Gupta <gshelly1402@gmail.com>
2018-10-14 23:40:15 +0530

    closures

--- closureExamples/ViewController.swift
+++ closureExamples/ViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var color = "blue"
+var weekday = "Monday"
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let closure = { [color] in
+            print("color is \(color)")
+            print("Today is \(self.weekday)")
+        }
+       color = "green"
+        weekday = "Tuesday"
+        closure()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    func applyTwice(f:((Float) -> Float), x:Float) -> Float {
+//
+//        let value = f(4.5)
+//        return 0.0
+//    }
 
 
 }
